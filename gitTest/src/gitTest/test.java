a853e537b9f807ce7000bc55a54003ab797c4114
smhrd <smhrd@DESKTOP-RQUD9EV>
2021-05-07 10:40:43 +0900

    수정 1차 추가

--- gitTest/src/gitTest/test.java
+++ gitTest/src/gitTest/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("dkdfkwjflwejo");
+		
+		System.out.println("¼öÁ¤ 1Â÷");
 	}
 
 }
